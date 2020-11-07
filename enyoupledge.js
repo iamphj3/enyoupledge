fc50427262d9144de3168e83b40818e543b44fa6
iamphj3 <73213437+iamphj3@users.noreply.github.com>
2020-11-08 00:05:13 +0900

    Update enyoupledge.js

--- enyoupledge.js
+++ enyoupledge.js
@@ -1,6 +1,6 @@
 
 var array=new Array(9);
- var post=document.getElementById("#post"),yes=document.getElementById("#yes"),no=document.getElementById("#no");
+ var post=document.getElementById("post"),yes=document.getElementById("yes"),no=document.getElementById("no");
  var count=0,flag=0;
  var i=0;
 
@@ -27,12 +27,12 @@ var array=new Array(9);
 
 
 
-document.getElementById("#post").text(question[count]);
-document.getElementById("#yes").text(pos[count]);
-document.getElementById("#no").text(neg[count]);
+document.getElementById("post").text(question[count]);
+document.getElementById("yes").text(pos[count]);
+document.getElementById("no").text(neg[count]);
 
 
-document.getElementById("#yes").click(function () {
+document.getElementById("yes").click(function () {
    if(count==6){alert("이미 모두 입력했습니다.");return;}
    switch(count){
       case 0: array[1]+=1; break;
@@ -43,12 +43,12 @@ document.getElementById("#yes").click(function () {
       case 5: array[3]+=1; break;
    }
    count++;
-   document.getElementById("#post").text(question[count]);
-   document.getElementById("#yes").text(pos[count]);
-   document.getElementById("#no").text(neg[count]);
+   document.getElementById("post").text(question[count]);
+   document.getElementById("yes").text(pos[count]);
+   document.getElementById("no").text(neg[count]);
 });
 
-document.getElementById("#no").click(function () {
+document.getElementById("no").click(function () {
    if(count==6){alert("이미 모두 입력했습니다.");return;}
    switch(count){
       case 0: array[0]+=1; array[6]+=1; break;
@@ -59,13 +59,15 @@ document.getElementById("#no").click(function () {
       case 5: array[6]+=2; break;
    }
    count++;
-   document.getElementById("#post").text(question[count]);
-   document.getElementById("#yes").text(pos[count]);
-   document.getElementById("#no").text(neg[count]);});
+   document.getElementById("post").text(question[count]);
+   document.getElementById("yes").text(pos[count]);
+   document.getElementById("no").text(neg[count]);});
 
 //yes,no에서 count가 6이면 호출할 함수
 function final() {
 
 }
+
+
 
 
